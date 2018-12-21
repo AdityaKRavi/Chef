007e13c60fea8185f05dac32857cf23c7a28cb71
AdityaKRavi <ravi981957632@gmail.com>
2018-12-21 07:06:39 +0000

    Gather node info using Ohai

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -5,7 +5,12 @@ package 'ntp'
 package 'git'
 
 file '/etc/motd' do
-  content 'This server is the property of Aditya K Ravi'
+  content "This server is the property of Aditya K Ravi
+    HOSTNAME : #{node['hostname']}
+    IPADDRESS : #{node['ipaddress']}
+    CPU : #{node['cpu']['0']['mhz']}
+    MEMORY : #{node['memory']['total']}
+  "
   action :create
   owner 'root'
   group 'root'
